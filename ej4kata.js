b181e746eb7c88929a6f513d738c0fbb446b2e76
Pedrojorgehg <hemijuegos4@gmail.com>
2024-05-03 13:09:40 +0200

    ejercicio 4
    finish

--- ej4kata.js
+++ ej4kata.js
@@ -4,13 +4,18 @@ console.log(avengers[0])
 
 /* 1.2 Cambia el primer elemento de avengers a "IRONMAN" */
 const avengers1 = ['HULK', 'SPIDERMAN', 'BLACK PANTHER']
+console.log((avengers1[0] = 'IRONMAN'))
+console.log(avengers1)
 
 /* 1.3 console numero de elementos en el array usando la propiedad correcta de Array. */
 const avengers2 = ['HULK', 'SPIDERMAN', 'BLACK PANTHER']
+console.log(avengers2.length)
 
 /* 1.4 Añade 2 elementos al array: "Morty" y "Summer".  
 Muestra en consola el último personaje del array*/
 const rickAndMortyCharacters = ['Rick', 'Beth', 'Jerry']
+rickAndMortyCharacters.push('Morty', 'Summer')
+console.log(rickAndMortyCharacters)
 
 /* 1.5 Elimina el último elemento del array y muestra el primero y el último por consola. */
 const rickAndMortyCharacters1 = [
@@ -21,6 +26,8 @@ const rickAndMortyCharacters1 = [
   'Summer',
   'Lapiz Lopez'
 ]
+rickAndMortyCharacters1.pop
+console.log(rickAndMortyCharacters1[0], rickAndMortyCharacters1[4])
 
 /* 1.6 Elimina el segundo elemento del array y muestra el array por consola. */
 const rickAndMortyCharacters2 = [
@@ -31,3 +38,5 @@ const rickAndMortyCharacters2 = [
   'Summer',
   'Lapiz Lopez'
 ]
+rickAndMortyCharacters2.splice(1, 1)
+console.log(rickAndMortyCharacters2)
