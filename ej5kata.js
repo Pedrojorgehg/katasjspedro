5e190f671f9c4d80ca5292e72dff74eeef61949e
Pedrojorgehg <hemijuegos4@gmail.com>
2024-05-03 14:15:54 +0200

    ejercicio 5 terminado
    go!!

--- ej5kata.js
+++ ej5kata.js
@@ -1,6 +1,12 @@
 const number1 = 10
 const number2 = 20
 const number3 = 2
+let y
+let j
+let x
+x = number3
+j = number2
+y = number1
 
 // ejemplo
 if (number1 === 10) {
@@ -19,14 +25,18 @@ if (number3 != number1) {
   console.log('number3 es distinto number1')
 }
 
-if ((number3 *= 5) == number1) {
+if ((x *= 5) == number1) {
   console.log('number3 por 5 es igual a number1')
 }
 
-/* if () {
-  console.log("number3 por 5 es igual a number1 Y number1 por 2 es igual a number2");
-} */
+if (((x *= 5) == number1, (y *= 2) == number2)) {
+  console.log(
+    'number3 por 5 es igual a number1 Y number1 por 2 es igual a number2'
+  )
+}
 
-/* if () {
-  console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
-} */
+if ((j /= 2) == number1 || (y /= 5) == number3) {
+  console.log(
+    'number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3'
+  )
+}
