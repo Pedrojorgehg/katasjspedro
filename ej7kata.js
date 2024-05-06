1dee6f3635f768c718c5d88170b4549d7bcb067c
Pedrojorgehg <hemijuegos4@gmail.com>
2024-05-06 12:07:49 +0200

    ej 7 ok
    ejercicio completado

---
+++ ej7kata.js
@@ -0,0 +1,19 @@
+function sum(numberOne, numberTwo) {
+  if (numberOne > numberTwo) {
+    console.log(numberOne)
+  } else {
+    console.log(numberTwo)
+  }
+}
+
+sum()
+function exam(x, y) {
+  if (x > y) {
+    console.log(x)
+  } else {
+    console.log(y)
+  }
+}
+let x = 6
+let y = 14
+exam(x, y)
