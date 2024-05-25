cd211ab5964ede9657450cd935fbf7cd09c801e5
Pedrojorgehg <hemijuegos4@gmail.com>
2024-05-25 14:51:02 +0200

    update ej9kata
    solucionado , elimino while y termino funcion con exito

--- ej9kata.js
+++ ej9kata.js
@@ -4,13 +4,9 @@ function sumAll(array) {
   let result = 0
   for (let i = 0; i < array.length; i++) {
     const element = array[i]
-    console.log(element)
-    while (i < array.length) {
-      /*  result += array[i]  */
-      result += element
-      console.log(result)
-    }
+    result += element
   }
+  return console.log(`El resultado es ` + result)
 }
 
 sumAll(numbers)
