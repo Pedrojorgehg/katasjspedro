f58b6ff3e1b0065f11de61cd1e4d87b4e0aacd79
Pedrojorgehg <hemijuegos4@gmail.com>
2024-05-25 15:04:45 +0200

    Create ej10kata
    creado y resuelto :)

---
+++ ej10kata.js
@@ -0,0 +1,12 @@
+const numbers = [12, 21, 38, 5, 45, 37, 6]
+function average(param) {
+  let result = 0
+  let sumArray = 0
+  for (let i = 0; i < param.length; i++) {
+    const element = param[i]
+    sumArray += element
+  }
+  result = sumArray / param.length
+  return console.log(`La meida es ` + result)
+}
+average(numbers)
