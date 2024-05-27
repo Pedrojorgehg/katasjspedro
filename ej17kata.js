c1a71425720cc3daf7321e344c96299b4a99db4f
Pedrojorgehg <hemijuegos4@gmail.com>
2024-05-27 16:44:34 +0200

    Update ej17kata.js
    uso bucle for in y muestro por consola los resultados

--- ej17kata.js
+++ ej17kata.js
@@ -6,3 +6,9 @@ const alien = {
   planet: 'Eden',
   weight: '259kg'
 }
+for (const key in alien) {
+  if (Object.hasOwnProperty.call(alien, key)) {
+    const element = alien[key]
+    console.log(element)
+  }
+}
