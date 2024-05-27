14f2826b34f461600025cc6b342be65f4f0af110
Pedrojorgehg <hemijuegos4@gmail.com>
2024-05-27 16:28:33 +0200

    Update ej13kata.js
    cambio color de los enunciados de los ejercicios para facilitar la correccion(colores solo visibles con las extension better comens de vs)

--- ej13kata.js
+++ ej13kata.js
@@ -1,3 +1,4 @@
+//*Buscador de nombres: Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe dentro de dicho array - comprueba si existe el elemento, en caso que existan nos devuelve un true y la posición de dicho elemento y por la contra un false. Puedes usar este array para probar tu función:
 const nameFinder = [
   'Peter',
   'Steve',
