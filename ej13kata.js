e8bc3c413989bf7f0e24255842282c8bd9af12df
Pedrojorgehg <hemijuegos4@gmail.com>
2024-05-27 12:05:48 +0200

    update ej13kata
    corrijo la variable positon , cambiandola a let y voy cambiando la posicion del console .log hasta que encuentro la linea enlaque solo me devuele un false , si no encuentra el parametro

--- ej13kata.js
+++ ej13kata.js
@@ -11,6 +11,16 @@ const nameFinder = [
   'Jessica',
   'Marc'
 ]
-function finderName(param) {
-  // insert code
+function finderName(param, name) {
+  for (let i = 0; i < param.length; i++) {
+    const element = param[i]
+    let positon = 0
+    if (element === name) {
+      positon = param.indexOf(element)
+      return console.log(true, `la posicion es ${positon}`)
+    }
+  }
+  console.log(false)
 }
+/* finderName(nameFinder, `paco`) */
+finderName(nameFinder, `Peggy`)
