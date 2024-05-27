5dc6d98a5790f366cebdbcf133100ecaad4cea79
Pedrojorgehg <hemijuegos4@gmail.com>
2024-05-27 16:27:42 +0200

    Update ej12kata.js
    cambio color de los enunciados de los ejercicios para facilitar la correccion(colores solo visibles con las extension better comens de vs)

--- ej12kata.js
+++ ej12kata.js
@@ -1,3 +1,6 @@
+//*Valores únicos: Crea una función que reciba por parámetro un array y compruebe si existen elementos duplicados, en caso que existan los elimina para retornar un array sin los elementos duplicados. Puedes usar este array para probar tu función:
+
+//!pista (cuando eliminamos algo de un array, su longitud se verá afectada)
 const duplicates = [
   'sushi',
   'pizza',
