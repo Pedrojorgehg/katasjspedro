319b3572b6c3ff461dec299b6da02ade4d78a3b1
Pedrojorgehg <hemijuegos4@gmail.com>
2024-05-27 15:56:24 +0200

    Update ej4kata.js
    cambio color de los enunciados de los ejercicios para facilitar la correccion(colores solo visibles con las extension better comens de vs)

--- ej4kata.js
+++ ej4kata.js
@@ -1,23 +1,23 @@
-/* 1.1 Consigue el valor "HULK" del array de avengers y muestralo por consola. */
+//* 1.1 Consigue el valor "HULK" del array de avengers y muestralo por consola. */
 const avengers = ['HULK', 'SPIDERMAN', 'BLACK PANTHER']
 console.log(avengers[0])
 
-/* 1.2 Cambia el primer elemento de avengers a "IRONMAN" */
+//* 1.2 Cambia el primer elemento de avengers a "IRONMAN" */
 const avengers1 = ['HULK', 'SPIDERMAN', 'BLACK PANTHER']
 console.log((avengers1[0] = 'IRONMAN'))
 console.log(avengers1)
 
-/* 1.3 console numero de elementos en el array usando la propiedad correcta de Array. */
+//* 1.3 console numero de elementos en el array usando la propiedad correcta de Array. */
 const avengers2 = ['HULK', 'SPIDERMAN', 'BLACK PANTHER']
 console.log(avengers2.length)
 
-/* 1.4 Añade 2 elementos al array: "Morty" y "Summer".  
-Muestra en consola el último personaje del array*/
+//* 1.4 Añade 2 elementos al array: "Morty" y "Summer".
+//*Muestra en consola el último personaje del array*/
 const rickAndMortyCharacters = ['Rick', 'Beth', 'Jerry']
 rickAndMortyCharacters.push('Morty', 'Summer')
 console.log(rickAndMortyCharacters)
 
-/* 1.5 Elimina el último elemento del array y muestra el primero y el último por consola. */
+//* 1.5 Elimina el último elemento del array y muestra el primero y el último por consola. */
 const rickAndMortyCharacters1 = [
   'Rick',
   'Beth',
@@ -29,7 +29,7 @@ const rickAndMortyCharacters1 = [
 rickAndMortyCharacters1.pop
 console.log(rickAndMortyCharacters1[0], rickAndMortyCharacters1[4])
 
-/* 1.6 Elimina el segundo elemento del array y muestra el array por consola. */
+//* 1.6 Elimina el segundo elemento del array y muestra el array por consola. */
 const rickAndMortyCharacters2 = [
   'Rick',
   'Beth',
