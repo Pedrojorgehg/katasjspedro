cd71761be2a537a08f9b50d8125e32456fcf1eb8
Pedrojorgehg <hemijuegos4@gmail.com>
2024-05-29 11:49:03 +0200

    Update ej24kata.js
    creo variavle result y le voy sumando cada valor de sellcount que contiene procut y retorna valor de result

--- ej24kata.js
+++ ej24kata.js
@@ -5,4 +5,12 @@ const products = [
   { name: 'Sable laser FX', sellCount: 23 },
   { name: 'Varita de Voldemort', sellCount: 6 }
 ]
-function sellCount(params) {}
+function sellCount(params) {
+  let resul = 0
+  for (let i = 0; i < params.length; i++) {
+    const element = params[i]
+    resul += element.sellCount
+  }
+  return console.log(resul)
+}
+sellCount(products)
